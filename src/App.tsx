be393589cdeb43de5f6077952790a57a1b2cce72
André Mena <63565529+agm02@users.noreply.github.com>
2025-06-23 10:52:14 +0000

    page folder renamed to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css';
-import Body from './pages/body';
-import Footer from './pages/footer';
-import Header from './pages/header';
+import "./App.css";
+import Body from "./layout/body";
+import Footer from "./layout/footer";
+import Header from "./layout/header";
 
 function App() {
-    return (
-        <div className="app">
-            <div className="main-content">
-                <Header />
-                <Body />
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="main-content">
+        <Header />
+        <Body />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
